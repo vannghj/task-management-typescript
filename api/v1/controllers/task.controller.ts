dd958512320ef0614997d291edff3aefd5c217cb
vannghj <131614984+vannghj@users.noreply.github.com>
2024-08-16 15:18:12 +0700

    loc trang thai

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -2,9 +2,17 @@ import {Request, Response} from "express";
 import Task from "../models/task.model";
 
 export const index = async (req: Request, res:Response) =>{
-    const tasks = await Task.find({
+    interface Find {
+        deleted: boolean,
+        status?: string,
+    }
+    const find: Find = {
         deleted: false,
-    })
+    };
+    if(req.query.status) {
+        find.status = req.query.status.toString();
+    }
+    const tasks = await Task.find(find);
     res.json(tasks);
 }
 
